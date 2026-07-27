1a61a5010d27cefcc29f5e24bb600ecc1d4a38ec
Võ Đình Long <you@example.com>
2026-07-27 11:46:17 +0700

    fix(ci): use classpath main class launcher for mvnw and system mvn for backend build in GitHub Actions

--- seatflow-backend/mvnw.cmd
+++ seatflow-backend/mvnw.cmd
@@ -1,53 +1,24 @@
-@REM ----------------------------------------------------------------------------
-@REM Licensed to the Apache Software Foundation (ASF) under one
-@REM or more contributor license agreements.  See the NOTICE file
-@REM distributed with this work for additional information
-@REM regarding copyright ownership.  The ASF licenses this file
-@REM to you under the Apache License, Version 2.0 (the
-@REM "License"); you may not use this file except in compliance
-@REM with the License.  You may obtain a copy of the License at
-@REM
-@REM    https://www.apache.org/licenses/LICENSE-2.0
-@REM
-@REM Unless required by applicable law or agreed to in writing,
-@REM software distributed under the License is distributed on an
-@REM "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-@REM KIND, either express or implied.  See the License for the
-@REM specific language governing permissions and limitations
-@REM under the License.
-@REM ----------------------------------------------------------------------------
-
-@REM ----------------------------------------------------------------------------
-@REM Maven Start Up Batch script
-@REM ----------------------------------------------------------------------------
-
 @echo off
 setlocal
 
-set MAVEN_CMD_LINE_ARGS=%*
-if "%[1]"=="[]" goto run
-
-:run
 set MAVEN_PROJECTBASEDIR=%~dp0
 if not "%MAVEN_PROJECTBASEDIR%"=="" set MAVEN_PROJECTBASEDIR=%MAVEN_PROJECTBASEDIR:~0,-1%
 
 set WRAPPER_JAR="%MAVEN_PROJECTBASEDIR%\.mvn\wrapper\maven-wrapper.jar"
-set WRAPPER_LAUNCHER=org.apache.maven.wrapper.MavenWrapperMain
 
-if exist %WRAPPER_JAR% goto runWrapper
+where mvn >nul 2>nul
+if %ERRORLEVEL% EQU 0 (
+    mvn %*
+    goto end
+)
 
-echo Could not find %WRAPPER_JAR%, downloading it...
-java -cp "%MAVEN_PROJECTBASEDIR%\.mvn\wrapper\maven-wrapper.properties" org.apache.maven.wrapper.MavenWrapperMain %*
-if %ERRORLEVEL% NEQ 0 goto error
-goto runWrapper
+if exist %WRAPPER_JAR% (
+    java -cp %WRAPPER_JAR% org.apache.maven.wrapper.MavenWrapperMain %*
+    goto end
+)
 
-:runWrapper
-java -jar %WRAPPER_JAR% %MAVEN_CMD_LINE_ARGS%
-if %ERRORLEVEL% NEQ 0 goto error
-goto end
-
-:error
-set ERROR_CODE=1
+echo Error: Maven or maven-wrapper.jar not found.
+exit /B 1
 
 :end
-@endlocal & exit /B %ERROR_CODE%
+@endlocal
